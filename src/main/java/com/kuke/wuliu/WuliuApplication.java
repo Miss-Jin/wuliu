af66cda940e89707aaaea56beb9a3040b77aa062
jhs <274497476@qq.com>
2021-04-19 12:18:55 +0800

    2021.4.19 项目初始化

--- src/main/java/com/kuke/wuliu/WuliuApplication.java
+++ src/main/java/com/kuke/wuliu/WuliuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WuliuApplication {
-
+    //主启动类
     public static void main(String[] args) {
         SpringApplication.run(WuliuApplication.class, args);
     }
