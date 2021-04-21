40cfb60ba15c3d9adc87946ad745e7c866961655
jhs <274497476@qq.com>
2021-04-21 14:10:13 +0800

    2021.4.21 14:09

--- src/main/java/com/kuke/wuliu/pojo/Shelves.java
+++ src/main/java/com/kuke/wuliu/pojo/Shelves.java
@@ -3,9 +3,19 @@ package com.kuke.wuliu.pojo;
 public class Shelves {
     private Integer shelvesId;
 
-    private Integer shelvesSpartitionid;
+    private Integer shelvesFpartitionid;
 
     private String shelvesName;
+
+    private String shelvesRemarks;
+
+    private Integer shelvesSort;
+
+    private Integer shelvesState;
+
+    private Integer shelvesStorage;
+
+    private String shelvesSize;
 
     public Integer getShelvesId() {
         return shelvesId;
@@ -15,12 +25,12 @@ public class Shelves {
         this.shelvesId = shelvesId;
     }
 
-    public Integer getShelvesSpartitionid() {
-        return shelvesSpartitionid;
+    public Integer getShelvesFpartitionid() {
+        return shelvesFpartitionid;
     }
 
-    public void setShelvesSpartitionid(Integer shelvesSpartitionid) {
-        this.shelvesSpartitionid = shelvesSpartitionid;
+    public void setShelvesFpartitionid(Integer shelvesFpartitionid) {
+        this.shelvesFpartitionid = shelvesFpartitionid;
     }
 
     public String getShelvesName() {
@@ -29,5 +39,45 @@ public class Shelves {
 
     public void setShelvesName(String shelvesName) {
         this.shelvesName = shelvesName == null ? null : shelvesName.trim();
+    }
+
+    public String getShelvesRemarks() {
+        return shelvesRemarks;
+    }
+
+    public void setShelvesRemarks(String shelvesRemarks) {
+        this.shelvesRemarks = shelvesRemarks == null ? null : shelvesRemarks.trim();
+    }
+
+    public Integer getShelvesSort() {
+        return shelvesSort;
+    }
+
+    public void setShelvesSort(Integer shelvesSort) {
+        this.shelvesSort = shelvesSort;
+    }
+
+    public Integer getShelvesState() {
+        return shelvesState;
+    }
+
+    public void setShelvesState(Integer shelvesState) {
+        this.shelvesState = shelvesState;
+    }
+
+    public Integer getShelvesStorage() {
+        return shelvesStorage;
+    }
+
+    public void setShelvesStorage(Integer shelvesStorage) {
+        this.shelvesStorage = shelvesStorage;
+    }
+
+    public String getShelvesSize() {
+        return shelvesSize;
+    }
+
+    public void setShelvesSize(String shelvesSize) {
+        this.shelvesSize = shelvesSize == null ? null : shelvesSize.trim();
     }
 }
